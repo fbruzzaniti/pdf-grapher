417fe2aa450f4b9c1dda8b31bdcab131a944f189
frank.bruzzaniti <frank.bruzzaniti@gmail.com>
2014-04-30 11:45:22 +0000

    Added detection of non printable text in objects Added /EmbeddedFile to redNodeList[] Squashed pdf-parser errors when encountering files embeded with non-printable characters﻿  ﻿  ﻿

--- pdf-grapher.py
+++ pdf-grapher.py
@@ -17,6 +17,9 @@
 # History  
   #0.5 rev7 alligning version numbers with repo
   #0.5 rev8 added other indicators from PDFiD split between redNodeList[] (commmonly in malware) and yellowNodeList[] (suspicious)
+  #0.5 rev9 added detection of non printable text in objects
+  #0.5 rev9 added /EmbeddedFile to redNodeList[]
+  #0.5 rev9 squashed pdf-parser errors when encountering files embeded with non-printable characters			
 
 # BUGS:
   #If you get the error "Couldn't import dot_parser, loading of dot files will not be possible." try this:
@@ -28,16 +31,15 @@
   #Test with objects that have revisions.
   #Cache getObjType lookups in a list do pdf-parser is not called again later to resolve ref's
   #Tidy up code
-  #Create html template for obj's in log dir
+  #Create html template for obj's in log dir. E.g. click in node takes u to html links to pdf-parser output, raw dump, file results, hex, strings, etc
   #Test on windows
-  #Add stream extraction
-  #squash popen err printing on console E.e. pdf-parser errors (non-fatal) with some embedded files
+  #Add stream/binary object extraction
   #Automated analysis of node relationships/patterns E.g. /AA -> /JS is more suspicious than just /JS or /AA	
 
 
 import pydot, os, sys, shutil, argparse, subprocess, string
 
-redNodeList = ['/Encrypt','/AA','/OpenAction','/RichMedia','/Launch','/JS','/JavaScript'] #commonly found in malware
+redNodeList = ['/Encrypt','/AA','/OpenAction','/RichMedia','/Launch','/JS','/JavaScript','/EmbeddedFile'] #commonly found in malware
 yellowNodeList = ['Contains stream','/JBIG2Decode','/ObjStm','/XFA'] #suspicious
 
 #awesome argparse tute https://docs.python.org/2/howto/argparse.html
@@ -66,13 +68,17 @@ if not os.path.isfile('pdf-parser.py'):
 def toAscii(s): #filter out nom-printables caused by embedded files
 	return filter(lambda x: x in string.printable, s)
 
+def isPrintable(s): #test if all characters are printable (they all should be in a PDF file)
+	return all(c in string.printable for c in s)
+
 def getObjType(objNum): 
 
-	p = subprocess.Popen(["python", "pdf-parser.py",args.file,"-o",str(objNum.split()[0])], stdout=subprocess.PIPE) #Only uses major not revision number
+	p = subprocess.Popen(["python", "pdf-parser.py",args.file,"-o",str(objNum.split()[0])], stdout=subprocess.PIPE,stderr=subprocess.PIPE) #Only uses major not revision number
 	
 	if not args.n:
 		f = open(log_path + "obj/" + args.file + '.obj' + str(objNum.split()[0]),'w') #save extracted obj to file in log directory
-		f.writelines(os.popen("python pdf-parser.py " + args.file + " -o " + str(objNum.split()[0])))
+		p = subprocess.Popen(["python", "pdf-parser.py",args.file,"-o",str(objNum.split()[0])], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+		f.writelines(iter(p.stdout.readline,''))
 		f.close()	
 
 	for line in iter(p.stdout.readline,''):
@@ -84,8 +90,9 @@ def getObjType(objNum):
 
 graph = pydot.Dot(graph_type='digraph') #set graph type
 		
-for line in os.popen("python pdf-parser.py " + args.file): #I could just read the pdf as text, but pdf-parser outputs it's own parsed results so they might be saner
-	line = toAscii(line)
+p = subprocess.Popen(["python", "pdf-parser.py",args.file],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+for line in iter(p.stdout.readline,''):
+	#line = toAscii(line) #throw this back in if we start getting errors handling binary
 	if len(line.split()) == 3 and line.split()[0] == "obj": #Look for an object and if found set the obj variable to it's value I.e. "<obj> <rev>"
 		obj = str(line).replace("obj","").strip() #get object
 		objType = getObjType(obj) #Get object type
@@ -96,22 +103,23 @@ for line in os.popen("python pdf-parser.py " + args.file): #I could just read th
 		else:
 			objUrl = ''
 
-		graph.add_node(pydot.Node("Obj " + obj, style="filled", fillcolor="#00ff00", URL=(objUrl))) #add noded named "Obj <obj> <rev>				
+		graph.add_node(pydot.Node("Obj " + toAscii(obj), style="filled", fillcolor="#00ff00", URL=(objUrl))) #add noded named "Obj <obj> <rev>				
 	
-	for item in redNodeList:
-		if item in line: #look for javascript tags if so colour the node red
-			graph.add_node(pydot.Node("Obj " + obj, style="filled", fillcolor="red", URL=(objUrl))) #pdf-parse should take care of octal and hex obfuscation
-	
-	for item in yellowNodeList:
-		if item in line: #Look for stream tag if so colour the node yellow
-			graph.add_node(pydot.Node("Obj " + obj, style="filled", fillcolor="yellow", URL=(objUrl)))
-	
+	for item in yellowNodeList: #If list item found in line create yellow node
+		if item in line: 
+			graph.add_node(pydot.Node("Obj " + toAscii(obj), style="filled", fillcolor="yellow", URL=(objUrl)))
 
+
+	for item in redNodeList: #If list item found in line create red node
+		if item in line or not isPrintable(line): #if we find non printable text in the node mark it red
+			graph.add_node(pydot.Node("Obj " + toAscii(obj), style="filled", fillcolor="red", URL=(objUrl))) #pdf-parse should take care of octal and hex obfuscation
+	
+	
 	if len(line.split()) > 1 and line.split()[0] == "Referencing:": #Only print obj's that have ref's I.E. > 1
 		for item in line.split(","):
 			ref = item.replace("Referencing:","").replace("R","").strip() #get ref obj
 			ref = ref + " (" + str(getObjType(ref)) + ")" #Combine re and ref type into var ref
-			graph.add_edge(pydot.Edge("Obj " + obj, "Obj " + ref, label="")) #add edges
+			graph.add_edge(pydot.Edge("Obj " + toAscii(obj), "Obj " + toAscii(ref), label="")) #add edges
 
 
 # Create graph, format selected by user
@@ -123,7 +131,7 @@ if args.o == 'png':
 	graph.write_png(os.path.splitext(args.file)[0] + ".png")
 
 if args.o == 'vrml':
-	graph.write_vrml(os.path.splitext(args.file)[0] + ".vrml")
+	graph.write_vrml(os.path.splitext(args.file)[0] + ".vrml") #this is in here so I have a work excuse to buy an occulus rift
 
 if not args.o:
 	graph.write_svg(os.path.splitext(args.file)[0] + ".svg")
