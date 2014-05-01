c22a223fdc82f1474cf0fc64dcb7b10246868aa3
frank.bruzzaniti <frank.bruzzaniti@gmail.com>
2014-05-01 17:08:33 +0000

    Added filter option for objects (invokes pdf-parser -f) Renamed log dirs <filename>_logs﻿

--- pdf-grapher-0.6-alpha.py
+++ pdf-grapher-0.6-alpha.py
@@ -21,7 +21,11 @@
   #0.5 rev9 added /EmbeddedFile to redNodeList[]
   #0.5 rev9 squashed pdf-parser errors when encountering files embeded with non-printable characters
   #0.5 rev10 fixed a bug where Type: of node was not displaying because of changes I had made in Rev9
-  #0.6a rev1 Removed external calls to pdf-parser except for one, handling objs in memory to recify performance issues with large or insane PDFs			
+  #0.6a rev11 Removed external calls to pdf-parser except for one, handling objs in memory to recify performance issues with large or insane PDFs
+  #0.6a rev13 Added filter option for objects (invokes pdf-parser -f)
+  #0.6a rev13 Renamed log dirs <filename>_logs	
+
+ 				
 
 # BUGS:
   #If you get the error "Couldn't import dot_parser, loading of dot files will not be possible." try this:
@@ -45,22 +49,23 @@ redNodeList = ['/Encrypt','/AA','/OpenAction','/RichMedia','/Launch','/JS','/Jav
 yellowNodeList = ['Contains stream','/JBIG2Decode','/ObjStm','/XFA'] #suspicious
 
 #awesome argparse tute https://docs.python.org/2/howto/argparse.html
-parser = argparse.ArgumentParser(usage='pdf-grapher, graphs objects and references from .pdf files.\n\nGraph Legend:\nRed = Contains or references common malware elements\nYellow = Contains or references possible malware elements\nWhite = Referenced Obj not found\nGreen = No malware elements found\n\nWritten by Frank Bruzzaniti <frank.bruzzaniti@gmail.com>, no Copyright.\nThis program is free software: you can redistribute it and/or modify it\nunder the terms of the GNU General Public License.\nUse at your own risk.\n')
+parser = argparse.ArgumentParser(usage='Graphs PDF objects and references for malware analysis.\npdf-parser.py -h for help\n\nGraph Legend:\nRed = Contains or references common malware elements\nYellow = Contains or references possible malware elements\nWhite = Referenced Obj not found\nGreen = No malware elements found\n\nWritten by Frank Bruzzaniti <frank.bruzzaniti@gmail.com>, no Copyright.\nThis program is free software: you can redistribute it and/or modify it\nunder the terms of the GNU General Public License.\nUse at your own risk.\n')
 
 parser.add_argument('file', help='pdf to graph')
-parser.add_argument('-o',type=str, help="graph output file format (default: svg)", choices=['dot', 'png', 'vrml'])
+parser.add_argument('-f', help='pass stream object through filter (FlateDecode,\nASCIIHexDecode, ASCII85Decode, LZWDecode', action='store_true')
+parser.add_argument('-o',type=str, help="graph output file format (default: svg)", choices=['dot','png','vrml'])
 parser.add_argument('-n', help='no obj log files or directories created', action='store_true')
 args = parser.parse_args()
 
 if not args.n:
 
-	log_path = "./" + args.file + "_log/" #set log path for extracted elements
+	logPath = "./" + args.file + "_logs/" #set log path for extracted elements
 
-	if os.path.exists(log_path): #check if log dir already exsists, if so delete it
-		shutil.rmtree(log_path)
+	if os.path.exists(logPath): #check if log dir already exsists, if so delete it
+		shutil.rmtree(logPath)
 
-	os.mkdir(log_path) #create new log directories	
-	os.mkdir(log_path + "obj/")
+	os.mkdir(logPath) #create new log directories	
+	os.mkdir(logPath + "obj/")
 
 if not os.path.isfile('pdf-parser.py'):
 	print 'pdf-grapher requires pdf-parser.py from http://blog.didierstevens.com/programs/pdf-tools\n'
@@ -83,8 +88,8 @@ def getObjType(objNum): #get's Type: field for opt
 			if len(line.split()) > 1:
 				return str(line.split()[1])
 
-def dmpObj(objNum): #dumps single object from pdfParserOut
-	stopList = ['xref','startxref','PDF Comment']
+def dmpObj(objNum): #dumps single object from pdfParserOut, I had a stopList but relying on 'pdf-parser -e i' now
+	#stopList = ['xref','startxref','PDF Comment','trailer']
 	objFound = False
 	returnList = []
 
@@ -96,17 +101,20 @@ def dmpObj(objNum): #dumps single object from pdfParserOut
 		if 'obj ' in line and 'obj ' + objNum not in line:
 			objFound = False
 
-		if objFound:
-			for item in stopList:
-				if item in line:
-					objFound = False
+		#if objFound:
+		#	for item in stopList:
+		#		if item in line:
+		#			objFound = False
 			
 		if objFound:
 			returnList.append(line)
 	return returnList
 
+if args.f:
+	p = subprocess.Popen(["python", "pdf-parser.py",'-ei','-f',args.file], stdout=subprocess.PIPE,stderr=subprocess.PIPE) #pdf-parser (filtered) into variable pdfParserOut
+else:
+	p = subprocess.Popen(["python", "pdf-parser.py",'-ei',args.file], stdout=subprocess.PIPE,stderr=subprocess.PIPE) #pdf-parser (un-filtered) into variable pdfParserOut
 
-p = subprocess.Popen(["python", "pdf-parser.py",args.file], stdout=subprocess.PIPE,stderr=subprocess.PIPE) #reads output from pdf-parser into variable pdfParserOut
 pdfParserOut = p.stdout.readlines()
 
 graph = pydot.Dot(graph_type='digraph') #set graph type
@@ -117,7 +125,7 @@ for line in pdfParserOut:
 		obj = str(line).replace("obj","").strip() #get object
 	
 		if not args.n: #objs dont get wriiten to disk if -n argument is given by user
-			f = open(log_path + "obj/" + args.file + '.obj' + obj.split()[0],'w') #save extracted obj to file in log directory
+			f = open(logPath + "obj/" + args.file + '.obj' + obj.split()[0],'w') #save extracted obj to file in log directory
 			for line in dmpObj(obj):
 				f.writelines(line)
 			f.close()		
@@ -126,7 +134,7 @@ for line in pdfParserOut:
 		obj = obj + " (" + str(objType) + ")" #combine obj and type into var obj
 
 		if not args.n:
-			objUrl = '"' + log_path + "obj/" + args.file + '.obj' + obj.split()[0] + '"' #set local URL for extracted obj's
+			objUrl = '"' + logPath + "obj/" + args.file + '.obj' + obj.split()[0] + '"' #set local URL for extracted obj's
 		else:
 			objUrl = ''
 
@@ -159,15 +167,16 @@ if args.o == 'png':
 if args.o == 'vrml':
 	graph.write_vrml(os.path.splitext(args.file)[0] + ".vrml") #this is in here so I have a work excuse to buy an occulus rift
 
+#if args.o == 'html': #need someone with html/svg skills to help me add filename to page as well as pan/zoom, etc
+#	if args.n: #if -n selected just create html and svg in path (no html dirs)
+#		graph.write_svg(os.path.splitext(args.file)[0] + ".svg")
+#		htmlPage = '<html><title>' + args.file + '</title>Filename: ' + args.file + '<br><img src="'+ os.path.splitext(args.file)[0] + '.svg"/></html>'
+#	else:
+#		graph.write_svg(logPath + os.path.splitext(args.file)[0] + ".svg")
+#		htmlPage = '<html><title>' + args.file + '</title><embed src="'+ logPath + os.path.splitext(args.file)[0] + '.svg" type="image/svg+xml"</html>'
+#	fileHtml = open(os.path.splitext(args.file)[0] + ".html",'w')
+#	fileHtml.writelines(htmlPage)
+#	fileHtml.close()
+
 if not args.o:
 	graph.write_svg(os.path.splitext(args.file)[0] + ".svg")
-
-
-
-
-		
-		
-		
-
-
-
